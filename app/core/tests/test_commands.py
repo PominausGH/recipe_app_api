dcafae5a49dec9459e5695b8ee70acc636aac3b2
PominausGH <genmailing@gmail.com>
2024-05-11 20:06:50 +1000

    op

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,7 @@
 
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2opError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -25,7 +25,7 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
 
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        patched_check.side_effect = [Psycopg2opError] * 2 + \
             [OperationalError] * 3 + [True]
         
         call_command('wait_for_db')
