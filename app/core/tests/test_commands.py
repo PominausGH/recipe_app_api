b3c0d08be37b0efa5c6e7918231c7c4a9036ff57
PominausGH <genmailing@gmail.com>
2024-05-11 18:51:20 +1000

    psycop2g works now

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,7 @@
 
 from unittest.mock import patch
 
-from pyscopg2 import OperationalError as Psycopg2OpError
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
