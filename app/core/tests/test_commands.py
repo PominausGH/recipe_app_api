6f0226ae245cd1be244656f32ec477203b80bbfc
PominausGH <genmailing@gmail.com>
2024-05-11 19:23:34 +1000

    typo

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,21 +4,18 @@
 
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2OpError
+from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """Test commands
-    """
+    """Test commands """
     
     def test_wait_for_db_ready(self, patched_check):
         """ Test waiting for database to be ready."""
-
         patched_check.return_value = True
 
         call_command('Wait_for_db')
@@ -28,7 +25,7 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
 
-        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
         
         call_command('wait_for_db')
