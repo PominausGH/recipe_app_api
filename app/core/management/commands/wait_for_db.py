de6605152290e05612056a8433f56cef117c35bb
PominausGH <genmailing@gmail.com>
2024-05-12 20:38:05 +1000

    removed psy

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,9 +1,5 @@
-""" 
-django commnad to wait for db
-"""
 import time
 
-from psycopg2 import OperationalError as Psycopg2opError
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
@@ -14,14 +10,12 @@ class Command(BaseCommand):
     def handle(self, *args, **commands):
         """ entry point for command"""
         self.stdout.write('waiting for database')
-        # db_up = False
         db_conn = None
         while not db_conn:
             try:
                 # self.check(databases=['default'])
                 db_conn = connections['default']
-                db_up = True
-            except (Psycopg2opError, OperationalError):
+            except (OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 seconds...')
                 time.sleep(1)
 
