c9decab51dd0a3d23fdaa296321830309a69c102
Andrew Dainty <genmailing@gmail.com>
2026-01-14 09:00:02 +1100

    feat(frontend): add users API module

---
+++ frontend/src/api/users.js
@@ -0,0 +1,34 @@
+import client from './client';
+
+export const usersApi = {
+  async get(id) {
+    const response = await client.get(`/interaction/users/${id}/`);
+    return response.data;
+  },
+
+  async getRecipes(userId, params = {}) {
+    const response = await client.get('/recipe/recipes/', {
+      params: { ...params, author: userId },
+    });
+    return response.data;
+  },
+
+  async follow(userId) {
+    const response = await client.post(`/interaction/users/${userId}/follow/`);
+    return response.data;
+  },
+
+  async getFollowers(userId, page = 1) {
+    const response = await client.get(`/interaction/users/${userId}/followers/`, {
+      params: { page },
+    });
+    return response.data;
+  },
+
+  async getFollowing(userId, page = 1) {
+    const response = await client.get(`/interaction/users/${userId}/following/`, {
+      params: { page },
+    });
+    return response.data;
+  },
+};
